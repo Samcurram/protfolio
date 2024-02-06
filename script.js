c920c9b480280a7eb16524bd634ec432db790059
Abdus Salaam <142170529+Samcurram@users.noreply.github.com>
2024-02-06 20:31:37 +0530

    added index.html,media.css,script.js,style.css

---
+++ script.js
@@ -0,0 +1,7 @@
+
+     var typed=new Typed('.typing',{
+        strings:["Web Developer","Designer","Program Solving","Data Management"],
+        typeSpeed:80,
+        backSpeed:100,
+        loop:true
+    })
